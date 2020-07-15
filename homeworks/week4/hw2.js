a53a1065ebdcc3c5641cced2aa420f12699d7dcb
BoisonChang <dragoncres@gmail.com>
2020-07-15 17:58:09 +0800

    Week4 全部完成囉 test15

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,57 +1,107 @@
 const request = require('request');
 const process = require('process');
 
-request.get(
-  'https://lidemy-book-store.herokuapp.com/books/',
-  (error, response, body) => {
-    let content;
-    try {
-      content = JSON.parse(body);
-    } catch (exception) {
-      console.log(exception); // 錯誤處理
-    }
-    if (process.argv[2] === 'list') {
+const args = process.argv;
+const BASEURL = 'https://lidemy-book-store.herokuapp.com';
+
+const action = args[2];
+const params = args[3];
+
+function TryError(str) {
+  let content;
+  try {
+    content = JSON.parse(str);
+    return content;
+  } catch (exception) {
+    console.log(exception); // 錯誤處理
+  }
+}
+
+function listbook() {
+  request.get(
+    `${BASEURL}/books/`, (error, response, body) => {
+      const content = TryError(body);
+      if (error) {
+        return console.log('抓取失敗', error);
+      }
       for (let i = 0; i <= content.length - 1; i += 1) {
         console.log(`${content[i].id} ${content[i].name}`);
+      } return error;
+    },
+  );
+}
+
+function readbook(id) {
+  request.get(
+    `${BASEURL}/books/`, (error, response, body) => {
+      const content = TryError(body);
+      if (error) {
+        return console.log('讀取失敗', error);
       }
-    }
-    if (process.argv[2] === 'read' && process.argv[3] >= 0 && process.argv[3] <= content.length - 1) {
-      console.log(content[process.argv[3]].name);
-    }
-  },
-);
+      console.log(content[id].name);
+      return error;
+    },
+  );
+}
 
-if (process.argv[2] === 'delete') {
+function deletebook(id) {
   request.delete(
-    {
-      method: 'DELETE',
-      url: `https://lidemy-book-store.herokuapp.com/books/+${process.argv[3]}`,
+    `${BASEURL}/books/${id}`, (error) => {
+      if (error) {
+        return console.log('刪除失敗', error);
+      }
+      console.log('刪除成功');
+      return error;
     },
   );
 }
 
-if (process.argv[2] === 'create') {
-  request.post(
-    {
-      url: 'https://lidemy-book-store.herokuapp.com/books/',
-      form: {
-        name: `${process.argv[3]}`,
-      },
-    },
-    (error, response, body) => {
-      console.log(body);
-    },
-  );
-}
-
-if (process.argv[2] === 'update') {
-  request.patch({
-    url: `https://lidemy-book-store.herokuapp.com/books/+${process.argv[3]}`,
+function createbook(name) {
+  request.post({
+    url: `${BASEURL}/books/`,
     form: {
-      name: `${process.argv[4]}`,
+      name,
     },
-  },
-  (error, response, body) => {
-    console.log(JSON.parse(body));
+  }, (error) => {
+    if (error) {
+      return console.log('新增失敗', error);
+    }
+    console.log('新增成功！');
+    return error;
   });
+}
+
+function updatebook(id, name) {
+  request.patch({
+    url: `${BASEURL}/books/${id}`,
+    form: {
+      name,
+    },
+  }, (error) => {
+    if (error) {
+      return console.log('更新失敗', error);
+    }
+    console.log('更新成功！');
+    return error;
+  });
+}
+
+switch (action) {
+  case 'list':
+    listbook();
+    break;
+  case 'read':
+    readbook(params);
+    break;
+  case 'delete':
+    deletebook(params);
+    break;
+  case 'create':
+    createbook(params);
+    break;
+  case 'update':
+    updatebook(params, args[4]);
+    break;
+  default:
+    console.log('請重新輸入正確指令');
 }
